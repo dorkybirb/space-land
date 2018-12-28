d67097890db7961442546a513299d78e80f40bcf
Piyobee <piyobee@users.noreply.github.com>
2018-12-28 14:26:45 +0400

    created index, and styles for email/pass authentication wip

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPubz7qBtFbRowcHDmEgnhc-hSYZw4af0",
+    authDomain: "space-land.firebaseapp.com",
+    databaseURL: "https://space-land.firebaseio.com",
+    projectId: "space-land",
+    storageBucket: "space-land.appspot.com",
+    messagingSenderId: "384163793922"
+  };
+  firebase.initializeApp(config);
+});
