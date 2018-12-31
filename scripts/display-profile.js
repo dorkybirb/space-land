1a1c9a1ee76d5f4354330c5581f5cd4631c8cb8f
Piyobee <piyobee@users.noreply.github.com>
2018-12-31 19:46:21 +0400

    added script to display basic profile info

--- scripts/display-profile.js
+++ scripts/display-profile.js
@@ -13,19 +13,34 @@ $(document).ready(function(){
     // Firebase starter script END ------------
 
     //------------ User auth BEGIN
+    const txtUserdA = document.getElementById('txtUserdA');
     const txtUserEmail = document.getElementById('txtUserEmail');
+    const txtUserExp = document.getElementById('txtUserExp');
+    const txtUserCoins = document.getElementById('txtUserCoins');
     const btnLogout = document.getElementById('btnLogout');
 
     //Add a realtime listener
-    firebase.auth().onAuthStateChanged(user => {
-      if(user) { //user is logged in
-        console.log(user); //display user details
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser) { //user is logged in
+        console.log(firebaseUser); //display user details
         console.log('logged in user visited profile');
         btnLogout.classList.remove('hide');
 
+        //create user profile if not already created
+        var user = firebase.auth().currentUser;
         var currentUID = firebase.auth().currentUser.uid;
-        if (user.coins != null)
+        console.log(user.coins != null);
+        if (user.coins == null) {
           writeInitialUserData(currentUID, 'm2u', user.email);
+        }
+
+        //displays user info
+        return firebase.database().ref('/users/' + currentUID).once('value').then(function(snapshot) {
+          txtUserdA.innerHTML = snapshot.val().dAaccount;
+          txtUserEmail.innerHTML = snapshot.val().email;
+          txtUserExp.innerHTML = snapshot.val().userExp;
+          txtUserCoins.innerHTML = snapshot.val().coins;
+        });
 
       } else { //user is logged out
         console.log('logged out');
