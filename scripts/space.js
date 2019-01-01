a7dd586af4241cf45cecad42cb21bbb4879a520c
Piyobee <piyobee@users.noreply.github.com>
2019-01-01 20:39:09 +0400

    added a random coin generator

--- scripts/space.js
+++ scripts/space.js
@@ -27,9 +27,28 @@ $(document).ready(function(){
     giveExpToUser(10, uid);
   });
 
+  //when 'earn 10 exp' button is pressed
+  plusRandom.addEventListener('click', e => {
+    var uid = firebase.auth().currentUser.uid;
+    giveRandCoinsToUser(0, 20, uid);
+  });
+
   function giveCoinsToUser(coinAmt, userId) {
     return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
       var numCoins = snapshot.val().coins;
+      var newNumCoins = numCoins + coinAmt;
+      firebase.database().ref('users/' + userId).update({
+        coins: newNumCoins
+      });
+      console.log('gave '+coinAmt+' coins!');
+    });
+  }
+
+  //Gives user a random amt of coins
+  function giveRandCoinsToUser(min, max, userId) {
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var numCoins = snapshot.val().coins;
+      var coinAmt = getRandomInt(min, max);
       var newNumCoins = numCoins + coinAmt;
       firebase.database().ref('users/' + userId).update({
         coins: newNumCoins
@@ -49,8 +68,7 @@ $(document).ready(function(){
     });
   }
 
-  function getRandomInt(max) {
-    // return Math.floor(Math.random() * (max - min + 1)) + min;
+  function getRandomInt(min, max) { // [min, max] both inclusive
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
